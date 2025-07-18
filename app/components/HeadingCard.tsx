01c168f348efa1f118084994dce69fbea647545d
Abubakr Rizan <74781555+aboo212031@users.noreply.github.com>
2025-07-18 05:27:29 +0000

    feat: heading card

---
+++ app/components/HeadingCard.tsx
@@ -0,0 +1,11 @@
+export default function HeadingCard() {
+  return (
+    <div className="flex flex-col gap-2 p-6">
+        <p className="text-4xl">Sports Xi Project</p>
+        <p className=" text-[#B1B5C3]">Event Production</p>
+        <p className="text-[#B1B5C3]">assigned</p>
+        <hr className="w-screen"></hr>
+        <div className="text-[#B1B5C3]">last updated on: 04 April, 2022</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HeadingCard from "./components/HeadingCard";
 
 export default function Home() {
   return (
-    <div>
+    <div className="fixed top-20 left-95">
+      <HeadingCard />
     </div>
   );
 }
